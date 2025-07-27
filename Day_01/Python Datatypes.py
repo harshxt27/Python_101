846c14ce29545959950e5851e2d7bd2612f2c9e4
harshxt27 <harshit.msj@Gmail.com>
2025-07-27 23:34:17 +0530

    Day1_commits

---
+++ Day_01/Python Datatypes.py
@@ -0,0 +1,52 @@
+#Example of text datatype in Python
+x = "Hello, World!"
+#x is a string
+
+
+#Example of integer datatype in Python
+y = 5
+#y is an integer    
+
+#Example of float datatype in Python
+z = 3.14
+#z is a float
+
+#Example of boolean datatype in Python
+a = True
+#b is a boolean
+
+#Example of list datatype in Python
+my_list = [1, 2, 3, 4, 5]
+#my_list is a list
+
+#Example of tuple datatype in Python
+my_tuple = (1, 2, 3)
+#my_tuple is a tuple
+
+#Example of dictionary datatype in Python
+my_dict = {"name": "Alice", "age": 30}
+#my_dict is a dictionary
+
+#Example of set datatype in Python
+my_set = {1, 2, 3}
+#my_set is a set
+
+#Example of None datatype in Python
+my_none = None
+#my_none is NoneType
+
+#Example of complex datatype in Python
+my_complex = 1 + 2j
+#my_complex is a complex number
+
+print("String:", x)
+print("Integer:", y)
+print("Float:", z)
+print("Boolean:", a)
+print("List:", my_list)
+print("Tuple:", my_tuple)
+print("Dictionary:", my_dict)
+print("Set:", my_set)
+print("NoneType:", my_none)
+print("Complex Number:", my_complex)
+
