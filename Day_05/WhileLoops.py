a4098ec2c187def35b5de6893d6fb61532b9f52b
harshxt27 <harshit.msj@Gmail.com>
2025-08-03 23:04:09 +0530

    Day_05

---
+++ Day_05/WhileLoops.py
@@ -0,0 +1,18 @@
+# While Loops
+# Initializing a counter
+counter = 0
+
+while counter < 5:
+    print("Counter:", counter)
+    counter += 1
+# This will print the counter value from 0 to 4
+
+# The loop continues until the counter reaches 5
+
+# Using a while loop to iterate through a list
+mylist = [10, 20, 30, 40, 50]
+index = 0
+while index < len(mylist):
+    print("List item:", mylist[index])
+    index += 1
+# This will print each item in the list until all items are printed
