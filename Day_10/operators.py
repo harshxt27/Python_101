e7c48c4f3df316807839c005dd9ce33e576670a7
harshxt27 <harshit.msj@Gmail.com>
2025-08-09 22:59:30 +0530

    Day_10

---
+++ Day_10/operators.py
@@ -0,0 +1,29 @@
+# Arithmetic Operators
+
+# Addition
+result = 5 + 3
+print("5 + 3 =", result)
+
+# Subtraction
+result = 5 - 3
+print("5 - 3 =", result)
+
+# Multiplication
+result = 5 * 3
+print("5 * 3 =", result)
+
+# Division
+result = 5 / 3
+print("5 / 3 =", result)
+
+# Modulus
+result = 5 % 3
+print("5 % 3 =", result)
+
+# Exponentiation
+result = 5 ** 3
+print("5 ** 3 =", result)
+
+# Floor Division
+result = 5 // 3
+print("5 // 3 =", result)
